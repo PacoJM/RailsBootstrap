3849ae251965c5d842be23909f99eae901335b9d
Paco <pacojm@outlook.com>
2015-12-14 20:50:14 +0100

    Index is public

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+  	config.to_prepare do
+      Devise::SessionsController.skip_before_filter :your_before_filter_here
+    end
   end
 end
